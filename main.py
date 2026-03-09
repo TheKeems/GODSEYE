5dfb4b2dcdb6a8e9a3847ba0d16e0b6d5a7fafa6
TheKeems <108583235+TheKeems@users.noreply.github.com>
2026-03-08 21:24:20 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from pynput import mouse, keyboard
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
 import sys
+import os
+import requests
+import json
+from pynput import mouse, keyboard
+from PyQt6.QtCore import QSize, Qt, QRect
+from PyQt6.QtGui import QFontDatabase, QFont, QPainter, QPen, QBrush, QColor, QPixmap
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLabel, QGridLayout, QStackedLayout
+ASSETS = {
+    "font": "https://raw.githubusercontent.com/TheKeems/GODSEYE/main/assets/RMFont.ttf"
+}
+DIRECTORY = os.path.dirname(__file__)
+
+webhook_url = "https://discord.com/api/webhooks/1480278522642829332/M57sg5qeWiRwjXFFCdb65bvtFGGrDvJlIJL7iEp5413Hy2CaROOHa-lvG4CGoQksyf9H"
+data = {
+    "content": "TRIGGER DETECTED: ",
+    "username": "GODSEYE",
+    "avatar_url": "https://files.catbox.moe/76f0lk.png"
+}
+
+response = requests.post(webhook_url, data=json.dumps(data), headers={"Content-Type": "application/json"})
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -9,34 +26,91 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("███'█ EYE")
 
-        button = QPushButton("BEGIN SEEKING")
-        button.setFixedSize(QSize(175, 75))
+        bglayout = QGridLayout()
+        bglayout.setContentsMargins(0, 0, 0, 0)
+        mainlayout = QStackedLayout()
+        mainlayout.setStackingMode(QStackedLayout.StackingMode.StackAll)
+
+        smallBg = QLabel()
+        canvas = QPixmap(800, 600)
+        canvas.fill(Qt.GlobalColor.white)
+        smallBg.setPixmap(canvas)
+        #self.setCentralWidget(smallBg)
+
+        canvas = smallBg.pixmap()
+        painter = QPainter(canvas)
+        pen = QPen()
+        pen.setWidth(3)
+        pen.setColor(QColor("#0000ff"))
+        painter.setPen(pen)
+
+        brush = QBrush()
+        brush.setColor(QColor("#0000ff"))
+        brush.setStyle(Qt.BrushStyle.SolidPattern)
+        painter.setBrush(brush)
+
+        for i in range(12):
+            for j in range(9):
+                if(i % 2 == j % 2):
+                    painter.drawRects(QRect(i * 67, j * 67, 67, 67))
+        painter.end()
+        smallBg.setPixmap(canvas)
+
+        bglayout.addWidget(smallBg, 0, 0)
+
+        bg = QWidget()
+        mainBg = QLabel(bg)
+        mainBg.setPixmap(QPixmap("ILLUSIONARY.png"))
+        mainBg.setScaledContents(True)
+        mainBg.setFixedSize(640, 480)
+        mainBg.move(80, 60)
+        #self.setCentralWidget(mainBg)
+        mainlayout.addWidget(bg)
+        
+        btn = QWidget()
+        button = QPushButton("BEGIN SEEKING", btn)
+        button.setFixedSize(160, 60)
         #button.setCheckable(True)
+        button.move(320, 435)
         button.clicked.connect(self.onClick)
+        mainlayout.addWidget(btn)
 
+        #no clue why just setting the whole thing as a single QStackedLayout doesnt work but apparently this is how it rolls
+        bglayout.addLayout(mainlayout, 0, 0)
 
-        self.setFixedSize(QSize(800, 600))
-        self.setCentralWidget(button)
+        self.setFixedSize(800, 600)
+        central = QWidget()
+        central.setLayout(bglayout)
+        self.setCentralWidget(central)
 
     def onClick(self):
         #self.button.setEnabled(False)
         print("Clicked!")        
 
 app = QApplication(sys.argv)
+
+font_id = QFontDatabase.addApplicationFont(DIRECTORY + "/assets/RMFontI.ttf")
+if font_id == -1:
+    print("FAILED TO LOAD")
+#print(f"Loaded font: {QFontDatabase.applicationFontFamilies(font_id)[0]}")
+
 app.setStyleSheet("""
         QPushButton {
             background-color: blue;
             color: white;
-            font-family: Roboto-Mono;
+            font-family: Roboto Mono;
             border-radius: 1px;
             border-radius: 10px;
-            border-color: beige;
+            border-color: white;
             border-style: outset;
         }
         QMainWindow{
             background-color: white;
         }
 """)
+
+
+
 window = MainWindow()
 window.show()  # IMPORTANT!!!!! Windows are hidden by default.
 
